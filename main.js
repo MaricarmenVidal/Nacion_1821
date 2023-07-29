0bea209515a34040169e9187473a54dc9d52baba
Maricarmen Vidal <maricarmenvidal204@gmail.com>
2023-07-29 17:55:55 -0500

    pedido

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
-const todo= document.querySelector(".todo")
-const entrada= document.querySelector(".entrada-filter")
-const plato_fondo= document.querySelector(".plato-filter")
-const postre= document.querySelector(".postre-filter")
-const bebida= document.querySelector(".bebidas-filter")
+let platosPedidos=[]
+let total=0
 const cards = document.querySelector(".cards")
 const card = document.querySelectorAll(".card")
 const filterBotones =document.querySelectorAll(".filtrado")
+const pedido=document.querySelector('.pedido')
+const DOMpedido = document.querySelector('#lista-pedido');
+const DOMtotal = document.querySelector('#total');
+const DOMbotonVaciar = document.querySelector('#boton-vaciar');
 
-const entradas=document.querySelectorAll('.entrada')
+const miLocalStorage = window.localStorage
+
 
 function Plato(id, nombre, descripcion, precio, categoria, image){
     this.id=id
@@ -24,8 +26,12 @@ const lomo_saltado=new Plato (3, "Lomo Saltado", "Jugosos trozos de lomo al wok,
 const tallarin_saltado = new Plato (4,"Tallarín Saltado", "Plato típico de la gastronomía peruana, propio de la cocina chifa con pollo, ají amarillo, cebolla china y tomates salteados al wok.", 17, "plato-fondo", "images/tallarin_saltado.jpg")
 const mazamorra_morada =new Plato(5, "Mazamorra Morada", "Postre típico de nuestra gastronomía elaborado a base de maíz morado concentrado con fécula.", 2, "postre", "images/mazamorra_morada.jpeg")
 const arroz_con_leche =new Plato(6, "Arroz con Leche", "Postre típico de nuestra gastronomía elaborado a base de arroz cocido en leche con azúcar y canela, servido con una pizca de canela en polvo y leche condesada", 3, "postre", "images/arroz_con_leche.jpg")
+const chicha_morada=new Plato(7, "Chicha Morada", "Concentrado de maíz morado, piña, manzana, membrillo, clavo de olor, canela y aceite esencial de limón", 7, "bebida", "images/chicha-morada.jpg")
+const limonada_tropical=new Plato (8, "Limonada Tropical", "Fresa, piña golden y zumo de limón", 6, "bebida", "images/limonada-tropical.jpg")
 
-const platos = [menestron, sopa_minuta, lomo_saltado, tallarin_saltado, mazamorra_morada, arroz_con_leche]
+const platos = [menestron, sopa_minuta, lomo_saltado, tallarin_saltado, mazamorra_morada, arroz_con_leche, chicha_morada, limonada_tropical]
+
+
 
 window.addEventListener("DOMContentLoaded", ()=>{
     displayMenuItems(platos)
@@ -57,10 +63,110 @@ function displayMenuItems(menuItems){
                 <h5 class="card-title card-titulo">${item.nombre}</h5>
                 <p class="card-texto">${item.descripcion}</p>
                 <p class="card-texto"> Precio <span>S/. ${item.precio}.00</span> </p>
-                <a href="#" class="btn btn-primary boton-anadir">Pedir</a>
+                <a href="#" class="btn btn-primary boton-agregar" data-id=${item.id}>Pedir</a>
             </div>
         </article> `
     })
     displayMenu=displayMenu.join("")
     cards.innerHTML=displayMenu
 }
+
+cards.addEventListener('click', agregarProductos)
+
+function agregarProductos(evt){
+    evt.preventDefault()
+    if(evt.target.classList.contains('boton-agregar')){
+        platosPedidos.push(evt.target.getAttribute('data-id'))
+        calcularTotal();
+        renderizarCarrito();
+        guardarPedidoEnLocalStorage();
+        displayMenuItems(platos)
+    }
+}
+
+function renderizarCarrito() {
+    cards.textContent = '';
+    const carritoSinDuplicados = [...new Set(platosPedidos)];
+    carritoSinDuplicados.forEach((item) => {
+        const miItem = platos.filter((itemBaseDatos) => {
+            return itemBaseDatos.id === parseInt(item);
+        });
+        const numeroUnidadesItem = platosPedidos.reduce((total, itemId) => {
+            return itemId === item ? total += 1 : total;
+        }, 0);
+        const miNodo = document.createElement('li');
+        miNodo.classList.add('list-group-item', 'text-right', 'mx-2');
+        miNodo.textContent = `${numeroUnidadesItem} x ${miItem[0].nombre} - S/. ${miItem[0].precio}`;
+        const miBoton = document.createElement('button');
+        miBoton.classList.add('btn', 'btn-danger', 'mx-5', `${miItem[0].id}`);
+        miBoton.textContent = 'X';
+        miBoton.style.marginLeft = '1rem';
+        miBoton.dataset.item = item;
+        miBoton.addEventListener('click', borrarItemPedido);
+        miNodo.appendChild(miBoton);
+        DOMpedido.appendChild(miNodo);
+    });
+}
+
+
+function borrarItemPedido(evt) {
+
+    evt.preventDefault();
+    console.log(evt.target.parentElement)
+    if(evt.target.classList.contains('btn-danger')){
+
+        const id = evt.target.dataset.item
+
+        platosPedidos = platosPedidos.filter((pedidoId) => {
+            return pedidoId !== id;
+        });
+    displayMenuItems(platos)
+
+    renderizarCarrito();
+    calcularTotal();
+    guardarPedidoEnLocalStorage();
+    }
+
+}
+
+function calcularTotal() {
+    total = 0;
+    platosPedidos.forEach((item) => {
+        const miItem = platos.filter((itemBaseDatos) => {
+            return itemBaseDatos.id === parseInt(item);
+        });
+        total = total + miItem[0].precio;
+    });
+    DOMtotal.textContent = total.toFixed(2);
+}
+
+
+function vaciarCarrito() {
+    platosPedidos = [];
+    renderizarCarrito();
+    calcularTotal();
+    localStorage.clear();
+    displayMenuItems(platos)
+    DOMpedido.textContent = '';
+
+}
+
+function guardarPedidoEnLocalStorage () {
+    miLocalStorage.setItem('pedido', JSON.stringify(platosPedidos));
+}
+
+function cargarPedidoDeLocalStorage () {
+    if (miLocalStorage.getItem('pedido') !== null) {
+
+        platosPedidos = JSON.parse(miLocalStorage.getItem('pedido'));
+    }
+}
+
+// Eventos
+DOMbotonVaciar.addEventListener('click', vaciarCarrito);
+
+// Inicio
+cargarPedidoDeLocalStorage();
+calcularTotal();
+renderizarCarrito();
+
